e40f3c712601f935e9f8f5816f601b3bf3dcfbd3
Prajwalgn-07 <prajwalg@testvagrant.com>
2021-07-04 16:10:44 +0530

    created headless mode for chrome and firefox driver

--- src/main/java/TestScripts/AddingToCartTest.java
+++ src/main/java/TestScripts/AddingToCartTest.java
@@ -3,7 +3,10 @@ package TestScripts;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import pages.CartPage;
 import pages.Homepage;
 import pages.LongPrintedDressPage;
@@ -14,10 +17,19 @@ import java.util.concurrent.TimeUnit;
 
 public class AddingToCartTest {
     public static void main(String[] args) throws IOException {
-//        WebDriverManager.firefoxdriver().setup();
-//        WebDriver driver=new FirefoxDriver();
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxBinary firefoxBinary=new FirefoxBinary();
+        firefoxBinary.addCommandLineOptions("--headless");
+        FirefoxOptions firefoxOptions=new FirefoxOptions();
+        firefoxOptions.setBinary(firefoxBinary);
+        WebDriver driver=new FirefoxDriver(firefoxOptions);
+//        WebDriverManager.chromedriver().setup();
+//        ChromeOptions options=new ChromeOptions();
+//        /*chrome options default size is mobile window, if we don't mention window size it will
+//          open in mobile size window and it may generate element not found exception*/
+//        options.addArguments("window-size=1400,800");
+//        options.addArguments("headless");
+//        WebDriver driver=new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get("https://www.google.com");
         driver.get("http://automationpractice.com/index.php");
--- src/main/java/TestScripts/CreateAccountTest.java
+++ src/main/java/TestScripts/CreateAccountTest.java
@@ -4,7 +4,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.json.simple.parser.ParseException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import pages.CreateAccountPage;
 import pages.Homepage;
 import pages.LoginPage;
@@ -14,10 +17,19 @@ import java.util.concurrent.TimeUnit;
 
 public class CreateAccountTest {
     public static void main(String[] args) throws IOException, ParseException {
-//        WebDriverManager.firefoxdriver().setup();
-//        WebDriver driver=new FirefoxDriver();
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxBinary firefoxBinary=new FirefoxBinary();
+        firefoxBinary.addCommandLineOptions("--headless");
+        FirefoxOptions firefoxOptions=new FirefoxOptions();
+        firefoxOptions.setBinary(firefoxBinary);
+        WebDriver driver=new FirefoxDriver(firefoxOptions);
+//        WebDriverManager.chromedriver().setup();
+//        ChromeOptions options=new ChromeOptions();
+//        /*chrome options default size is mobile window, if we don't mention window size it will
+//          open in mobile size window and it may generate element not found exception*/
+//        options.addArguments("window-size=1400,800");
+//        options.addArguments("headless");
+//        WebDriver driver=new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get("https://www.google.com");
         driver.get("http://automationpractice.com/index.php");
@@ -29,6 +41,7 @@ public class CreateAccountTest {
             loginPage.TypeEmail();
             loginPage.ClickCreateAccount();
             createAccountPage.FillDetails();
+            System.out.println(driver.getTitle());
 //            createAccountPage.submitDetails();
         }
         finally {
